630dcff1405430f75b9bb41921ab44b3dbef26b0
AidanIbrahim25 <aidanibrahim28@gmail.com>
2026-06-02 18:26:29 +0800

    Fix: removed eventmanagement prefix from root url config

--- EventManagement/urls.py
+++ EventManagement/urls.py
@@ -22,7 +22,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('eventmanagement/', include('EventProject.urls'))
+    path('', include('EventProject.urls')) 
 ]
 
 # Add media URL patterns for development
